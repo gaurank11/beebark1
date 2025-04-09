a48c2a7435809e9eab5db343ab014ad6ea63c133
Gaurank Sharma <95328684+gaurank11@users.noreply.github.com>
2025-04-09 13:33:15 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,17 +8,15 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 import JoinUs from '../components/joinus';
+import ReferralSection from '../components/refer';
 
 
 const Register = () => {
   return (
     <main className="bg-white">
       <Header />
-     
       <JoinUs />
-    
-      
-      <Footer />
+      <ReferralSection />
     </main>
   )
 }
